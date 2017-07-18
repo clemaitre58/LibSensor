4230be740f8118a4da223fac72eb28ba1d58ac88
Cédric Lemaitre <c.lemaitre58@gmail.com>
2017-07-18 17:08:57 +0200

    calibration et constructeur OK

--- Sensor/bme280.py
+++ Sensor/bme280.py
@@ -1,3 +1,4 @@
+import time
 # Default I2C address for device.
 BME280_I2CADDR_DEFAULT        = 0x76
 
@@ -90,9 +91,127 @@ BME280_T_SB_20   = 0b111
 
 # filter coef
 
-BME_FILTER_OFF = 0b000
-BME_FILTER_2 = 0b001
-BME_FILTER_4 = 0b010
-BME_FILTER_8 = 0b011
-BME_FILTER_16 = 0b100
+BME280_FILTER_OFF = 0b000
+BME280_FILTER_2 = 0b001
+BME280_FILTER_4 = 0b010
+BME280_FILTER_8 = 0b011
+BME280_FILTER_16 = 0b100
+
+
+class BME280(object):
+
+    """Class which allows manipulate the BME280 conponent from bosch"""
+
+    def __init__(self, mode=BME280_MODE_NORMAL, osrs_h = BME280_OSRS_H_1, 
+            osrs_p = BME280_OSRS_P_1, osrs_t = BME280_OSRS_T_1, 
+            coef_filter = BME280_FILTER, address = BME280_I2CADDR_DEFAULT, 
+            i2c = None, standby = BME280_T_SB_1000, **kwargs):
+        """TODO: to be defined1.
+
+        :mode: TODO
+        :osrs_h: TODO
+        :osrs_p: TODO
+        :osrs_t: TODO
+        :coef_filter: TODO
+        :address: TODO
+        :i2c: TODO
+        :**kwargs: TODO
+
+        """
+        # check if mode value is correct
+        if mode not in [BME280_MODE_SLEEP, BME280_MODE_FORCED, BME280_MODE_NORMAL]:
+            raise VaueError(
+                    'Unexpected mode value {0}.'.format(mode))
+        self._mode = mode
+        # check if osrs_h value is correct
+        if osrs_h not in [BME280_OSRS_H_SKIPPED, BME280_OSRS_H_1, BME280_OSRS_H_2, 
+                BME280_OSRS_H_2, BME280_OSRS_H_4, BME280_OSRS_H_8, BME280_OSRS_H_16]:
+            raise VaueError(
+        self._osrs_h = osrs_h
+                    'Unexpected osrs_h value {0}.'.format(mode))
+        # check if osrs_h value is correct
+        if osrs_p not in [BME280_OSRS_P_SKIPPED, BME280_OSRS_P_1, BME280_OSRS_P_2, 
+                BME280_OSRS_P_2, BME280_OSRS_P_4, BME280_OSRS_P_8, BME280_OSRS_P_16]:
+            raise VaueError(
+                    'Unexpected osrs_p value {0}.'.format(mode)) 
+        self._osrs_p = osrs_p
+        # check if osrs_t value is correct
+        if osrs_t not in [BME280_OSRS_T_SKIPPED, BME280_OSRS_T_1, BME280_OSRS_T_2, 
+                BME280_OSRS_T_2, BME280_OSRS_T_4, BME280_OSRS_T_8, BME280_OSRS_T_16]:
+            raise VaueError(
+                    'Unexpected osrs_t value {0}.'.format(mode)) 
+        self._osrs_t = osrs_t
+        # check if coef_filter value is correct
+        if coef_filter not in [BME280_FILTER_OFF, BME280_FILTER_2, BME280_FILTER_4, 
+                BME280_FILTER_8, BME280_FILTER_16]:
+            raise VaueError(
+                    'Unexpected coef_filter value {0}.'.format(mode)) 
+        self._coef_filter = coef_filter
+        # check if adderess value is correct
+        if address not in [BME280_I2CADDR_DEFAULT, 0x77]:
+            raise VaueError(
+                    'Unexpected address value {0}.'.format(mode)) 
+        self._address = address
+        # check if adderess value is correct
+        if standby not in [BME280_T_SB_0_5, BME280_T_SB_62_5, BME280_T_SB_125, 
+                BME280_T_SB_250, BME280_T_SB_500, BME280_T_SB_1000, BME280_T_SB_10, BME280_T_SB_20]:
+            raise VaueError(
+                    'Unexpected stanby value {0}.'.format(mode)) 
+        self._standby = stanby
+        # create i2c device
+        if i2c is None:
+            import Adafruit_GPIO.I2C as I2C
+            i2c = I2C
+        # create I2C Device
+        try:
+            self._device = i2c.get_i2c_device(self._address, **kwargs)
+        except IOError :
+            print("Unable to communicate with Sensor")
+            exit()
+        # Load calibration valuesi
+        self._load_calibration()
+
+        # Config component
+        self._device.write8(BME280_CTRL_MEAS, 0x24)
+        time.sleep(0.002)
+        self._device.write8(BME280_CONFIG, ((self._standby << 5) | (self._coef_filter << 2)))
+        time.sleep(0.002)
+        self._device.write8(BME280_CRTL_HUM, self._osrs_h)
+        self._device.write8(BME280_CTRL_MEAS,((self._osrs_t << 5) | (self._osrs_p << 2) | 3))
+        self.temp = 0.0
+
+
+
+        def _load_calibration(self):
+            """Load Calibration of the factory in order to compute real value
+            :returns: TODO
+
+            """
+            self.dig_T1 = self._device.readU16LE(BME280_DIG_T1)
+            self.dig_T2 = self._device.readU16LE(BME280_DIG_T2)
+            self.dig_T3 = self._device.readU16LE(BME280_DIG_T3)
+
+            self.dig_P1 = self._device.readU16LE(BME280_DIG_P1)
+            self.dig_P2 = self._device.readU16LE(BME280_DIG_P2)
+            self.dig_P3 = self._device.readU16LE(BME280_DIG_P3)
+            self.dig_P4 = self._device.readU16LE(BME280_DIG_P4)
+            self.dig_P5 = self._device.readU16LE(BME280_DIG_P5)
+            self.dig_P6 = self._device.readU16LE(BME280_DIG_P6)
+            self.dig_P7 = self._device.readU16LE(BME280_DIG_P7)
+            self.dig_P8 = self._device.readU16LE(BME280_DIG_P8)
+            self.dig_P9 = self._device.readU16LE(BME280_DIG_P9)
+
+            self.dig_H1 = self._device.readU8(BME280_DIG_H1)
+            self.dig_H2 = self._device.readU16LE(BME280_DIG_H2)
+            self.dig_H3 = self._device.readU8(BME280_DIG_H3)
+            self.dig_H6 = self._device.readU8(BME280_DIG_H7)
+
+            h4 = self._device.readS8(BME280_DIG_H4)
+            h4 = (h4<<4)
+            self.dig_H4 = h4 | (self._device.readU8(BME280_DIG_H5) & 0x0F)
+
+            h5 = self._device.readS8(BME280_DIG_H6)
+            h5 = (h5 << 4)
+            self.dig_H5 = h5 | (self._device.readU8(BME280_DIG_H5) >> 4 & 0x0F)
+
 
